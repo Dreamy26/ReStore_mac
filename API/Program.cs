91734b0ee27a4ce195e380530ddae91019ba66ea
Dreamy <dreamy@Dreamys-MBP.attlocal.net>
2022-04-05 12:32:32 -0400

    adds product seed

--- API/Program.cs
+++ API/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -13,7 +16,21 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope(); //garbage collection
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>(); // will be used to log errors to terminal
+            try
+            {
+                context.Database.Migrate(); // database creation
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data.");
+            }
+            
+            host.Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
