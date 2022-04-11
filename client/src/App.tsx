0e52702660cf035ae4002b1026e67944241bd778
Dreamy <dreamy@Dreamys-MBP.attlocal.net>
2022-04-11 14:14:05 -0400

    adds useEffect & useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
+import {useEffect, useState} from 'react';
 
 
-function App() {
-  return (
-    <div >
-     <h1>Re-Store</h1>
-    </div>
-  );
+function App(){
+    const [products, setProducts] = useState([
+        {name: 'product1', price: 100.00},
+        {name: 'product2', price: 200.00},
+    ]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products') // fetch data from our api
+          .then(response => response.json())// a promise
+          .then(data => setProducts(data)); // set the data to our state
+    }, []); // empty array adding a dependency, this effect will only run once
+
+function addProduct(){
+    setProducts(prevState => [...prevState, 
+    {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
 }
-
+return (
+    <div> 
+    <h1>Re-Store</h1>
+    <ul>
+        {products.map((item, index) => (
+            <li key={index}>{item.name} - {item.price}</li>
+        ))}
+    </ul>
+    <button onClick={() => addProduct()}>Add Product</button>
+    </div>
+);
+ }
+    
 export default App;
