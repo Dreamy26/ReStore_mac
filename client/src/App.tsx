fb96a466a082e462a3e3c74d5bf8ffd47fa3b637
Dreamy <dreamy26@icloud.com>
2022-04-15 14:20:00 -0400

    Updates products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,26 @@
 import {useEffect, useState} from 'react';
+import { Product } from './product';
 
 
 function App(){
-    const [products, setProducts] = useState([
-        {name: 'product1', price: 100.00},
-        {name: 'product2', price: 200.00},
-    ]);
+    const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/api/products') // fetch data from our api
           .then(response => response.json())// a promise
-          .then(data => setProducts(data)); // set the data to our state
-    }, []); // empty array adding a dependency, this effect will only run once
+          .then(data => setProducts(data)) // set the data to our state
+    }, []) // empty array adding a dependency, this effect will only run once
 
 function addProduct(){
     setProducts(prevState => [...prevState, 
-    {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+    {
+        id: prevState.length + 101,
+        name: 'Product ' + (prevState.length + 1),
+        price: (prevState.length *100) + 100,
+        brand: 'some brand',
+        description: 'some description',
+        pictureUrl: 'https://picsum.photos/200'
+    }])
 }
 return (
     <div> 
